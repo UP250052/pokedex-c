5519449417e74e5a5d572e755dd8c6583a1c66e1
UP250052 <up250052@alumnos.upa.edu.mx>
2026-03-02 15:30:34 -0600

    add getPokemns fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,31 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+    useEffect(() => {
+    console.log("Entre en pantalla ");
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    try {
+      const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const response = await fetch(URL, {
+      method: "GET",
+    });
+
+    if (response.ok) {
+      console.log("Request okey ");
+    } else {
+      console.log("Bad request ");
+    }
+  } catch (error) {
+    console.log("Error en la petición");
+  }
+};
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
